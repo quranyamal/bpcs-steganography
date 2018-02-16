2b7fac821e5caacc81dbb862730f56caa6fb021b
quranyamal <mal.qurany@gmail.com>
2018-02-16 22:27:52 +0700

    add method load image, get image blocks/chunks

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Bit-Plane Complexity Segmentation (BPCS) Steganography
 
-This is a Netbeans project implementats BPCS Steganography algorithm
+This is a Netbeans project implements BPCS Steganography algorithm
--- src/bpcs/steganography/BpcsSteganography.java
+++ src/bpcs/steganography/BpcsSteganography.java
@@ -1,21 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bpcs.steganography;
 
-/**
- *
- * @author malqu
- */
-public class BpcsSteganography {
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 
-    /**
-     * @param args the command line arguments
-     */
+public class BpcsSteganography {
+    
+    private static int CHUNK_HEIGHT = 8;
+    private static int CHUNK_WIDTH = 8;
+    
+    private BufferedImage loadImage(String pathToFile) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(pathToFile));
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return image;
+    }
+    
+    private void printImageProperties(BufferedImage image) {
+        if (image!=null) {
+            System.out.println("Image Height:\t " + image.getHeight());
+            System.out.println("Image Width :\t " + image.getWidth());
+            System.out.println("Image Type  :\t " + image.getType());
+            System.out.println("Pixel Size  :\t " + image.getColorModel()
+                    .getPixelSize());
+            System.out.println(image.getColorModel());
+        } else {
+            System.out.println("Image is not loaded");
+        }
+    }
+    
+    private BufferedImage[] getImageChunks(BufferedImage image) {
+        int cols = image.getWidth()/CHUNK_WIDTH;
+        int rows = image.getHeight()/CHUNK_HEIGHT;
+        BufferedImage imageChunks[] = new BufferedImage[rows*cols];
+        
+        int count = 0;
+        for (int x=0; x<rows; x++) {
+            for (int y=0; y<cols; y++) {
+                imageChunks[count] = new BufferedImage(CHUNK_WIDTH, CHUNK_HEIGHT, 
+                        image.getType());
+            }
+        }
+        return imageChunks;
+    }
+    
+    private BufferedImage getImagePlane(BufferedImage image, int planeIdx) {
+        return null;
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        BufferedImage coverImg;
+        
+        BpcsSteganography bpcs = new BpcsSteganography();
+        String imageFile = "gajah.jpg";
+        
+        coverImg = bpcs.loadImage(imageFile);
+        System.out.println("Cover image properties");
+        bpcs.printImageProperties(coverImg);
+        
+        BufferedImage[] coverImgChunks = bpcs.getImageChunks(coverImg);
+        
+        System.out.println("\nChunk-0 properties");
+        bpcs.printImageProperties(coverImgChunks[0]);
+        
     }
     
 }
